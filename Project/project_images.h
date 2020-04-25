060e045030b658224de70d5af434df9b1c91d002
Emily Cebasek <emilycebasek@outlook.com>
2020-04-25 17:16:02 -0500

    Added bitmap header file

--- Project/project_images.h
+++ Project/project_images.h
@@ -1,5 +1,9 @@
+#include <stdint.h>
+#include "bitmap_db.h"
+
+
 // Font data for Microsoft Sans Serif 8pt
-extern const uint_8 microsoftSansSerif_8ptBitmaps[];
+extern const uint8_t microsoftSansSerif_8ptBitmaps[];
 extern const FONT_INFO microsoftSansSerif_8ptFontInfo;
 extern const FONT_CHAR_INFO microsoftSansSerif_8ptDescriptors[];
 
